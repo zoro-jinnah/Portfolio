dc74152096568085a7ce753c0b5208fe3527c952
zoro-jinnah <mohammedalijinna46@gmail.com>
2024-01-18 15:20:21 +0530

    ss

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".context",{
-    strings:["Web Designer","UI/UX Designer","Mobile App Developer"],
+    strings:["Web Designer","UI/UX Designer","Frontend Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
